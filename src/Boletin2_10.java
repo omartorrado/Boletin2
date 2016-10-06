d9cdcb9057404858255b15f71a12b72b36e24a05
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-10-06 11:43:49 +0200

    cambio de int a long para que soporte cantidades mayores

--- src/Boletin2_10.java
+++ src/Boletin2_10.java
@@ -17,12 +17,18 @@ public class Boletin2_10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int total=Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos euros tiene?"));
-        int b100=total/100;
-        int b20=(total-b100*100)/20;
-        int b5=(total-b100*100-b20*20)/5;
-        int b1=(total-b100*100-b20*20-b5*5);
+        long total=Long.parseLong(JOptionPane.showInputDialog("¿Cuantos euros tiene?"));
+        long b100=(total/100);
+        long b20=((total-b100*100)/20);
+        long b5=((total-b100*100-b20*20)/5);
+        long b1=((total-b100*100-b20*20-b5*5));
         JOptionPane.showMessageDialog(null, "Se podria desglosar en: \n Billetes de 100: "+b100+"\n Billetes de 20: "+b20+"\n Billetes de 5: "+b5+"\n Monedas de 1: "+b1);
+        System.out.println(b100*100);
+        System.out.println(b20*20);
+        System.out.println(b5*5);
+        System.out.println(b1);
+        System.out.println(b100);
+        System.out.println(b20);
     }
     
 }
